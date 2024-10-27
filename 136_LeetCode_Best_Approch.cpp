48f8e7745291f2803a4793b7e3c587e3c457a6e7
harsh-agr20 <138716860+harsh-agr20@users.noreply.github.com>
2024-10-28 03:00:06 +0530

    Optimal

---
+++ 136_LeetCode_Best_Approch.cpp
@@ -0,0 +1,11 @@
+//single no.
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xor1 =0;
+        for(int i=0;i<nums.size();i++){
+            xor1 = xor1 ^ nums[i];            
+        }
+        return xor1;   
+    }
+};
